8cb9636df6357ab5358f37acddcdd61f7e7c95e4
minhtri123442 <minhtrichautran@gmail.com>
2025-06-20 00:00:14 +0700

    update view

--- ViewCinema/CinemaProject/CinemaProject/Controllers/CinemaViewController.cs
+++ ViewCinema/CinemaProject/CinemaProject/Controllers/CinemaViewController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CinemaProject.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CinemaProject.Controllers
 {
@@ -6,7 +7,24 @@ namespace CinemaProject.Controllers
     {
         public IActionResult Home()
         {
-            return View();
+            ViewBag.Movies = new List<Movie>
+            {
+                new Movie { Id = 1, Title = "Dưới đáy hồ", PosterUrl = "/images/Phim Dưới đáy hồ.jpg", Duration = "1h18" },
+                new Movie { Id = 2, Title = "Doraemon Movie 44", PosterUrl = "/images/Nobita và thế giới trong tranh.jpg", Duration = "2h00" }
+            };
+
+            ViewBag.Cinemas = new List<Cinema>
+            {
+                new Cinema { Id = 1, Name = "Beta Quang Trung", Area = "TP. Hồ Chí Minh" }
+            };
+
+            ViewBag.Showtimes = new List<Showtime>
+            {
+                new Showtime { Id = 1, MovieId = 1, CinemaId = 1, Time = DateTime.Parse("2025-06-07T13:00:00") },
+                new Showtime { Id = 2, MovieId = 2, CinemaId = 1, Time = DateTime.Parse("2025-06-07T08:00:00") }
+            };
+
+            return View("Views/CinemaView/Home.cshtml");
         }
     }
 }
