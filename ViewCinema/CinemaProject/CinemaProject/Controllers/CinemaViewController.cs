ba0009863d2e391bd6ba7f004d17cf724ec4a794
minhtri123442 <minhtrichautran@gmail.com>
2025-06-19 22:07:42 +0700

    View

---
+++ ViewCinema/CinemaProject/CinemaProject/Controllers/CinemaViewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaProject.Controllers
+{
+    public class CinemaViewController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+    }
+}
---
+++ ViewCinema/CinemaProject/CinemaProject/Models/Cinema.cs
@@ -0,0 +1,9 @@
+﻿namespace CinemaProject.Models
+{
+    public class Cinema
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Area { get; set; }
+    }
+}
