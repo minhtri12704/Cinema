9908f07f25f0c3f986601bcbe2d8d677fd922d5b
QuangP03 <phamhuyminhquang310@gmail.com>
2025-07-05 08:27:19 +0700

    ThucAnvaDoUong

---
+++ ViewCinema/CinemaProject/CinemaProject/Models/MonAnvaThucUong.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CinemaProject.Models
+{
+    public class MonAnvaThucUong
+    {
+        [Key]
+        public int MaMon { get; set; }
+
+        [Required]
+        public string TenMon { get; set; }
+
+        [Required]
+        public string Loai { get; set; }
+
+        [Required]
+        public decimal Gia { get; set; }
+
+        public string? MoTa { get; set; }
+
+        public bool TrangThai { get; set; } = true;
+    }
+}
